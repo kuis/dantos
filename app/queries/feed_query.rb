5a2d29b0c0ea764085ff50a1373f00ad388a00af
Hsu Pan <github.1up@0sg.net>
2016-08-19 19:47:57 +0800

    save

--- app/queries/feed_query.rb
+++ app/queries/feed_query.rb
@@ -2,6 +2,14 @@ class FeedQuery
   attr_reader :posts
 
   def initialize posts:, page:
-    @posts = posts.includes(:goomp, :user, :likers, comments: :user).order(created_at: :desc).page(page).per(5)
+    @posts = posts.includes(
+      :goomp,
+      :user,
+      :likers,
+      comments: [
+        :user,
+        :likers
+      ]
+    ).order(created_at: :desc).page(page).per(5)
   end
 end
