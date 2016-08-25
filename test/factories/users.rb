2ded256400e30cd92bd25b5eeb056e317149e852
Hsu <github.1up@0sg.net>
2016-08-25 09:23:36 +0800

    reduce the size of the like button fix #36 if like count is 0 don't show it. It should be a like button fix #31

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
+    headline { Faker::Lorem.word }
     picture { "http://lorempixel.com/200/200/cats/" }
     password "qwer1234"
     password_confirmation "qwer1234"
