c1e5cac1d61918611d2d7580219a9bc2d3027700
Hsu Pan <github.1up@0sg.net>
2016-08-28 09:22:38 +0800

    Scraper from status box from home page is not working fix #95

--- app/controllers/scrapes_controller.rb
+++ app/controllers/scrapes_controller.rb
@@ -1,5 +1,11 @@
 class ScrapesController < ApplicationController
   def create
     @link = Scraper.scrape(params[:url])
+    @post = Post.new(
+      link_title: @link[:title],
+      link_description: @link[:description],
+      link_image: @link[:image],
+      link_url: @link[:url]
+    )
   end
 end
--- app/views/scrapes/create.json.jbuilder
+++ app/views/scrapes/create.json.jbuilder
@@ -1,6 +1,6 @@
 json.extract! @link, :title, :url, :image, :description
 json.card render(
   partial: "posts/post_card",
-  locals: @link,
+  locals: { post: @post },
   formats: [:html]
 )
