43dbe5f39a49c8f845a483fcbc17c6c45e792dd7
Hsu Pan <github.1up@0sg.net>
2016-09-05 07:36:16 +0800

    save

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,11 @@
 class PagesController < ApplicationController
   def index
     if user_signed_in?
-      redirect_to current_user.rooms.first
-      # @messages = @room&.messages&.includes(:user) || []
-
-      # respond_to do |f|
-      #   f.html { render "dashboard", layout: "application" }
-      #   f.js { render "posts/infinite_posts" }
-      # end
+      if current_user.rooms.first
+        redirect_to current_user.rooms.first
+      else
+        render "dashboard", layout: "application"
+      end
     else
       render "index"
     end
