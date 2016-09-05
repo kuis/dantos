fac111bebd463c7ecad0fabb873ed1a91f0b72ed
Hsu Pan <github.1up@0sg.net>
2016-09-05 10:35:38 +0800

    save

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905003626) do
+ActiveRecord::Schema.define(version: 20160905023023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(version: 20160905003626) do
     t.string   "category_name"
     t.integer  "budget_cents",    default: 0,     null: false
     t.string   "budget_currency", default: "USD", null: false
+    t.integer  "sequence"
     t.index ["manager_id"], name: "index_rooms_on_manager_id", using: :btree
     t.index ["user_id"], name: "index_rooms_on_user_id", using: :btree
   end
