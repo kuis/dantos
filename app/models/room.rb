c0dd08b1be70fee6452e8c0f52d0e33c022b44fa
Hsu Pan <github.1up@0sg.net>
2016-09-05 09:31:07 +0800

    save

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  has_many :messages
+  has_many :messages, dependent: :destroy
   belongs_to :user
   belongs_to :manager, class_name: "User"
   monetize :budget_cents
