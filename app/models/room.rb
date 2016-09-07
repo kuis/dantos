d8aea1adf520eaedf365f4a55f3f178bcb51dd6c
Hsu Pan <github.1up@0sg.net>
2016-09-07 08:15:46 +0800

    save

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,20 +7,20 @@ class Room < ApplicationRecord
   validates_presence_of :category_name
   before_create { self.category_name ||= "Design" }
 
-  def room_name_for_manager
+  def room_name_for_manager user, index
     "# #{self.user.slug}-#{self.category_name.downcase}-#{index+1}"
   end
 
-  def room_name_for_client
+  def room_name_for_client user, index
     "# #{self.category_name&.downcase}-#{index+1}""# #{self.category_name&.downcase}-#{index+1}"
     "# #{self.user.slug}-#{self.category_name.downcase}-#{index+1}"
   end
 
-  def get_room_name_for_user user
+  def get_room_name_for_user user, index
     if user == self.user
-      room_name_for_client
+      room_name_for_client user, index
     else user == self.manager
-      room_name_for_manager
+      room_name_for_manager user, index
     end
   end
 end
