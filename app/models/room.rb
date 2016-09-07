bc12faf2c2ba97648e37629650a6257c4c6caf88
Hsu Pan <github.1up@0sg.net>
2016-09-07 08:07:47 +0800

    save

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,4 +6,21 @@ class Room < ApplicationRecord
 
   validates_presence_of :category_name
   before_create { self.category_name ||= "Design" }
+
+  def room_name_for_manager
+    "# #{self.user.slug}-#{self.category_name.downcase}-#{index+1}"
+  end
+
+  def room_name_for_client
+    "# #{self.category_name&.downcase}-#{index+1}""# #{self.category_name&.downcase}-#{index+1}"
+    "# #{self.user.slug}-#{self.category_name.downcase}-#{index+1}"
+  end
+
+  def get_room_name_for_user user
+    if user == self.user
+      room_name_for_client
+    else user == self.manager
+      room_name_for_manager
+    end
+  end
 end
