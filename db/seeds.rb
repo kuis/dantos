6ce09e4cc4fb3c7e3450cb145d047a3b3d978125
Dirceu Vieira <dirceuvjr@gmail.com>
2016-09-20 03:13:09 -0300

    temp commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
-User.destroy_all
+#User.destroy_all
+
+
+u = User.where(:email => 'manager@goomp.co').first
+u.update!({:picture => 'http://dantos-production.kta86kespy.us-west-2.elasticbeanstalk.com/favicon.ico'}) unless u.nil?
 
 FactoryGirl.create(
   :user,
@@ -10,4 +14,4 @@ FactoryGirl.create(
   :last_name => 'Goomp',
   :slug => 'goomp-manager',
   :headline => 'Manager',
-  :picture => 'http://goomp.co/assets/square-logo-055f9a0dcbf3ab38fb6f1af03e735779ed8ee85b9e64a85245bc01d3a5a8882f.ico') unless User.where(:email => 'manager@goomp.co').first
+  :picture => 'http://dantos-production.kta86kespy.us-west-2.elasticbeanstalk.com/favicon.ico') unless User.where(:email => 'manager@goomp.co').first
