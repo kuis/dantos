f29df30c87cd8919ce9a08d64f1252b61b6bd2f0
Dirceu Vieira <dirceuvjr@gmail.com>
2016-09-26 11:41:56 -0300

    remove user deletion.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-User.destroy_all
-
-
-u = User.where(:email => 'manager@goomp.co').first
-u.update!({:picture => 'http://dantos-production.kta86kespy.us-west-2.elasticbeanstalk.com/favicon.ico'}) unless u.nil?
-
 FactoryGirl.create(
   :user,
   :email => 'manager@goomp.co',
@@ -15,3 +9,6 @@ FactoryGirl.create(
   :slug => 'goomp-manager',
   :headline => 'Manager',
   :picture => 'http://dantos-production.kta86kespy.us-west-2.elasticbeanstalk.com/favicon.ico') unless User.where(:email => 'manager@goomp.co').first
+
+u = User.where(:email => 'manager@goomp.co').first
+u.update!({:picture => 'http://dantos-production.kta86kespy.us-west-2.elasticbeanstalk.com/favicon.ico'}) unless u.nil?
