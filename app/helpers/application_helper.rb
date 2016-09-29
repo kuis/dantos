de151d435ec861fab44ee0c99a98c8cf91c6ee2c
vibol <vibolteav@gmail.com>
2016-09-29 09:47:08 +0700

    Remove header for sign in page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def no_header?
+    controller_name == 'sessions'
+  end
+
+  def toggle_class_name(class_name, condition = true)
+    class_name if condition
+  end
 end
